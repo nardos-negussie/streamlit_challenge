54447e76f459b637b45c4f86cc80c78dea911876
nardos-negussie <nardosnegussie519461@gmail.com>
2025-06-24 10:16:31 +0300

    day 3: button

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 ''' 30 day streamlit challenge :
-day 2: hello world'''
+day 3: button'''
 #%%
 import streamlit as st
 
 # %%
-st.write("Hello world!")
+st.header("Button")
+left, right = st.columns(2)
+btn_reset = right.button("", icon=":material/sync:", type='tertiary')
+btn_hello = left.button("Hello", icon=":material/thumb_up:")
+if btn_hello:
+    st.write("Why hello there!")
+else:
+    st.write("Goodbye!")
 
 # %%
+
